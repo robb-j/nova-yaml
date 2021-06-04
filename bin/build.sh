7795da58792cab19d53ae8c96ad7b1236aa77fc6
Rob Anderson <robanderson@hey.com>
2021-06-04 23:24:00 +0000

    try bundling yaml server with esbuild

--- bin/build.sh
+++ bin/build.sh
@@ -7,3 +7,17 @@ npx esbuild \
   --platform=neutral \
   --outfile=yaml.novaextension/Scripts/main.dist.js \
   src/Scripts/main.ts
+
+# 
+# Try to bundle the YAML into a binary, this fails:
+# // node_modules/vscode-json-languageservice/lib/umd/services/jsonSchemaService.js
+# code:
+#   var Strings = require2("../utils/strings");
+# 
+# npx esbuild \
+#   --bundle \
+#   --format=cjs \
+#   --target=node10 \
+#   --platform=node \
+#   --outfile=yaml.novaextension/Scripts/yaml.dist.js \
+#   node_modules/yaml-language-server/out/server/src/server.js
