7bd3cad6712d172387ff04ee530203773c5fc0c6
Rob Anderson <robanderson@hey.com>
2022-06-13 22:25:15 +0100

    update dev dependencies

--- yaml.novaextension/Scripts/main.dist.js
+++ yaml.novaextension/Scripts/main.dist.js
@@ -2,24 +2,19 @@ var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
 var __export = (target, all) => {
   for (var name in all)
     __defProp(target, name, { get: all[name], enumerable: true });
 };
-var __reExport = (target, module2, copyDefault, desc) => {
-  if (module2 && typeof module2 === "object" || typeof module2 === "function") {
-    for (let key of __getOwnPropNames(module2))
-      if (!__hasOwnProp.call(target, key) && (copyDefault || key !== "default"))
-        __defProp(target, key, { get: () => module2[key], enumerable: !(desc = __getOwnPropDesc(module2, key)) || desc.enumerable });
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
   }
-  return target;
+  return to;
 };
-var __toCommonJS = /* @__PURE__ */ ((cache) => {
-  return (module2, temp) => {
-    return cache && cache.get(module2) || (temp = __reExport(__markAsModule({}), module2, 1), cache && cache.set(module2, temp), temp);
-  };
-})(typeof WeakMap !== "undefined" ? /* @__PURE__ */ new WeakMap() : 0);
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 var __async = (__this, __arguments, generator) => {
   return new Promise((resolve, reject) => {
     var fulfilled = (value) => {
@@ -47,6 +42,7 @@ __export(main_exports, {
   activate: () => activate,
   deactivate: () => deactivate
 });
+module.exports = __toCommonJS(main_exports);
 
 // src/Scripts/utils.ts
 var console2 = globalThis.console;
@@ -374,4 +370,3 @@ function errorHandler(error) {
 }
 nova.commands.register("robb-j.yaml.restart", (workspace) => restartCommand(workspace, langServer).catch(errorHandler));
 nova.commands.register("robb-j.yaml.generate-kube-schemas", (workspace) => generateKubeSchemasCommand(workspace).catch(errorHandler));
-module.exports = __toCommonJS(main_exports);
